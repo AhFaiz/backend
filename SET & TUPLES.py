2a4e0eeffed3a905033beb5cdcb185304e92a8de
AhFaiz <106593295+AhFaiz@users.noreply.github.com>
2022-07-15 13:21:52 +0700

    Add files via upload

---
+++ SET & TUPLES.py
@@ -0,0 +1,15 @@
+# tuples
+data_tuples = (7,8,9,10,11)
+print(data_tuples)
+print(data_tuples[3])
+
+# tidak bisa dilakukan
+#data_tuples[1] = "pace"
+# data_tuples.append(1)
+
+# sets (himpunan)
+data_sets = {10,4,3,2,4,7,6,5}
+print(data_sets)
+
+#gabisa
+#print(data_sets[2])
