56ebacc83211732318f33922f4f94d9609df5509
AhFaiz <106593295+AhFaiz@users.noreply.github.com>
2022-07-19 11:18:10 +0700

    Add files via upload

---
+++ DATA STRUCTURE/ARRAY D.1.py/Array.py
@@ -0,0 +1,25 @@
+#note 
+
+#ARRAY :
+# - variable yang menyimpan data
+# - data yang disimpan banyak (lebih dari satu)
+# - data yang disimpan memeiliki tipe yang sama
+
+#ARRAY D.1 :
+#- vektor : Baris ataupun Kolom
+#- akses data cukup dengan 1 index
+
+
+cars = ["Ford", "Volvo", "BMW"]
+
+x = cars[0] #access the index 0 from the cars
+print(x)
+
+cars[0] = "Ferrari" #modify the index item 0
+print(cars)
+
+x = len(cars) #to know how much the item in the cars
+print(x)
+
+
+
